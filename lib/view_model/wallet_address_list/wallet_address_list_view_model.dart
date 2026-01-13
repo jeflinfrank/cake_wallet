3f238408bb97195e9801eee664f0b6f60957523c
Robert Malikowski <malikowskirobert@gmail.com>
2026-01-13 21:57:22 +0100

    fix restricted import

--- lib/view_model/wallet_address_list/wallet_address_list_view_model.dart
+++ lib/view_model/wallet_address_list/wallet_address_list_view_model.dart
@@ -30,7 +30,6 @@ import 'package:cake_wallet/view_model/wallet_address_list/wallet_address_list_i
 import 'package:cake_wallet/view_model/wallet_address_list/wallet_address_util.dart';
 import 'package:cake_wallet/wownero/wownero.dart';
 import 'package:cake_wallet/zano/zano.dart';
-import 'package:cw_bitcoin/electrum_wallet_addresses.dart';
 import 'package:cw_core/amount_converter.dart';
 import 'package:cw_core/crypto_currency.dart';
 import 'package:cw_core/currency.dart';
@@ -523,7 +522,7 @@ abstract class WalletAddressListViewModelBase extends WalletChangeListenerViewMo
   @action
   Future<void> rotateAddress() async{
     await createNewAddress(wallet, "");
-    if(wallet.type == WalletType.monero || wallet.walletAddresses is ElectrumWalletAddresses) {
+    if(isElectrumWallet) {
       wallet.walletAddresses.address = (addressList.last as WalletAddressListItem).address;
     }
   }
